ff9a63ac3da59fdc59449ef1c3509a2f37f33af2
yucho123987 <78955732+yucho123987@users.noreply.github.com>
2021-02-13 19:08:17 +0800

    Create days.js

---
+++ days.js
@@ -0,0 +1,8 @@
+function dateDiff(startDateString, endDateString){
+    var separator = "-"; //日期分隔符
+    var startDates = startDateString.split(separator);
+    var endDates = endDateString.split(separator);
+    var startDate = new Date(startDates[0], startDates[1]-1, startDates[2]);
+    var endDate = new Date(endDates[0], endDates[1]-1, endDates[2]);
+    return parseInt(Math.abs(endDate - startDate ) / 1000 / 60 / 60 /24);//把相差的毫秒数转换为天数
+};
