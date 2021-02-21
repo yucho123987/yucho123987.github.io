acc03c24dbdb273021626b19409364773c1b0f89
yucho123987 <78955732+yucho123987@users.noreply.github.com>
2021-02-21 18:17:56 +0800

    Update jqextend.js

--- blogres/jqextend.js
+++ blogres/jqextend.js
@@ -10,5 +10,18 @@ var array=document.querySelectorAll(selector);
 for(i=0;i<array.length;i++){
 code(array[i])
 }
+},
+isMobile:function(){
+var mobile=["android","ios","strawberry"];
+for(i=0;i<mobile.length;i++){
+if(window.navigator.userAgent.toLowerCase().indexOf(mobile[i])>-1){
+var ismobi=true
+}
+}
+if(ismobi==true){
+return true;
+}else{
+return false;
+}
 }
 });
